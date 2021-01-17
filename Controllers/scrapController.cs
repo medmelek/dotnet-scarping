97cdf2ca74de70be66a4483a73ea36ca8bfa36dc
med melek <medmelek007@gmail.com>
2021-01-17 16:49:18 +0100

    first

---
+++ Controllers/scrapController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aspose.Words;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using scraping.Models;
+namespace scraping.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class scrapController : ControllerBase
+    {
+        
+        public scrapController(){}
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string uri1 = "https://www.mytek.tn/13-pc-portable?selected_filters=gamer-oui%2Ftype-ordinateur_portable&id_category=13&n=68";
+            string uri2 = "https://www.mytek.tn/13-pc-portable?selected_filters=gamer-oui%2Ftype-ordinateur_portable";
+                List<laptop> laptops = new List<laptop>();
+            int i = 1;
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb() ;
+            HtmlAgilityPack.HtmlDocument doc = web.Load(uri1);
+            //string m = doc.DocumentNode.SelectNodes("//img[@class='replace-2x img-responsive'").First().Attributes[""].Value;
+            //SelectNodes("//a/img")
+            foreach (var item in doc.DocumentNode.SelectNodes("//a[@class='product_img_link']/img"))
+            {
+                string src = item.Attributes["src"].Value;
+                laptop ilaptop = new laptop();
+                ilaptop.photo = src;
+                ilaptop.title = doc.DocumentNode.SelectNodes("//a[@class='product-name']")[i].InnerText;
+                laptops.Add(ilaptop);
+                i++;
+            }
+            return Ok(laptops);
+        }
+    }
+}
---
+++ Models/laptop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace scraping.Models
+{
+    public class laptop
+    {
+        public string title  { get; set; }
+        public string photo { get; set; }
+
+    }
+}
